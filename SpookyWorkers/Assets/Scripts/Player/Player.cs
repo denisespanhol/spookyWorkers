a12726ccd43d00fde663e668538ce096375628a6
Denis Espanhol <denisgfespanhol@gmail.com>
2023-08-21 14:40:21 -0300

    Changing the jump that the player can jump only if they groundCheck is touching the floor

--- SpookyWorkers/Assets/Scripts/Player/Player.cs
+++ SpookyWorkers/Assets/Scripts/Player/Player.cs
@@ -7,22 +7,29 @@ public class Player : MonoBehaviour
     #region VARIABLES
 
     [Header("Movement Setup")]
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
     [SerializeField] private float speedWalk = 2f;
     [SerializeField] private float jumpForce;
 
-    private Rigidbody2D playerRigidbody2D;
+    private Rigidbody2D _playerRigidbody2D;
+    private bool _isTouchingTheFloor;
 
     #endregion
 
     private void Awake()
     {
-        playerRigidbody2D = GetComponent<Rigidbody2D>();
+        _playerRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         HandleMovement();
         HandleJump();
+
+        // the Physics2D.OverlapCircle return a boolean: true if the groundCheck object inside the Player object is touching the floor and false if its not
+        _isTouchingTheFloor = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 
     // function to control the players horizontal movement
@@ -30,20 +37,21 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
-            playerRigidbody2D.velocity = new Vector2(speedWalk, playerRigidbody2D.velocity.y);
+            _playerRigidbody2D.velocity = new Vector2(speedWalk, _playerRigidbody2D.velocity.y);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            playerRigidbody2D.velocity = new Vector2(-speedWalk, playerRigidbody2D.velocity.y);
+            _playerRigidbody2D.velocity = new Vector2(-speedWalk, _playerRigidbody2D.velocity.y);
         }
     }
 
+    // function to the player jump if them its not in the air
     private void HandleJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _isTouchingTheFloor)
         {
-            playerRigidbody2D.velocity = Vector2.up * jumpForce;
+            _playerRigidbody2D.velocity = Vector2.up * jumpForce;
         }
     }
 
