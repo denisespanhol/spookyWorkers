24c9c6ea3e2af1b0e36d72bb6787f3e2cad63f24
Denis Espanhol <denisgfespanhol@gmail.com>
2023-08-28 13:50:23 -0300

    Creating PauseGame.cs

---
+++ SpookyWorkers/Assets/Scripts/Utils/PauseGame.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public void Pause()
+    {
+        Time.timeScale = 1 - Time.timeScale;
+
+        if (Time.timeScale == 0) Time.timeScale = 1;
+
+        else if (Time.timeScale == 1) Time.timeScale = 0;
+    }
+}
